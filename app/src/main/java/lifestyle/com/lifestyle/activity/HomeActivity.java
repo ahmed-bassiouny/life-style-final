4fa9052a3c12c46b1be2511620a13472600fcb0b
bassiouny <bassiouny>
2018-10-29 21:18:16 +0200

    home screen

---
+++ app/src/main/java/lifestyle/com/lifestyle/activity/HomeActivity.java
@@ -0,0 +1,17 @@
+package lifestyle.com.lifestyle.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lifestyle.com.lifestyle.R;
+import lifestyle.com.lifestyle.base.ui.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideStatusBar();
+        setContentView(R.layout.activity_home);
+    }
+}
