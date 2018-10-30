332c709ed34a8d8b562e85a5be65d8fd9441d693
ahmed <ahmedbassiouny26@gmail.com>
2018-10-30 14:55:13 +0200

    authentacation with google

--- app/src/main/java/lifestyle/com/lifestyle/controller/LoginController.java
+++ app/src/main/java/lifestyle/com/lifestyle/controller/LoginController.java
@@ -2,9 +2,11 @@ package lifestyle.com.lifestyle.controller;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 
 import bassiouny.ahmed.genericmanager.SharedPrefManager;
+import lifestyle.com.lifestyle.R;
 import lifestyle.com.lifestyle.api.RequestCallback;
 import lifestyle.com.lifestyle.base.ui.BaseController;
 import lifestyle.com.lifestyle.helper.Constants;
@@ -13,11 +15,26 @@ import lifestyle.com.lifestyle.interactor.UserInteractor;
 import lifestyle.com.lifestyle.model.User;
 
 public class LoginController extends BaseController {
+
     private IUserInteractor interactor;
+    private FirebaseAuth mAuth;
+    private GoogleSignInOptions gso ;
+
+
 
     public LoginController(Activity activity, Fragment fragment) {
         super(activity, fragment);
         interactor = new UserInteractor();
+        initGoogle();
+    }
+
+    private void initGoogle(){
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void login(String email, String password) {
@@ -42,7 +59,27 @@ public class LoginController extends BaseController {
             showMessage(msg);
             getFragment().endLoading();
         }
-
-        ;
     };
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
---
+++ app/src/main/java/lifestyle/com/lifestyle/fragments/SignInFragment.java
@@ -0,0 +1,67 @@
+package lifestyle.com.lifestyle.fragments;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import lifestyle.com.lifestyle.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SignInFragment extends Fragment {
+
+    private int RC_SIGN_IN = 123;
+
+
+    public SignInFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+}
