bca1f50d5bbc5b97d2c32a4051b1eb9359075c5e
bassiouny <bassiouny>
2018-11-07 01:54:20 +0200

    home

--- app/src/main/java/lifestyle/com/lifestyle/activity/HomeActivity.java
+++ app/src/main/java/lifestyle/com/lifestyle/activity/HomeActivity.java
@@ -2,12 +2,33 @@ package lifestyle.com.lifestyle.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import bassiouny.ahmed.genericmanager.SharedPrefManager;
+import butterknife.BindView;
 import lifestyle.com.lifestyle.R;
 import lifestyle.com.lifestyle.base.ui.BaseActivity;
+import lifestyle.com.lifestyle.helper.Constants;
 import lifestyle.com.lifestyle.helper.DefaultValue;
 
-public class HomeActivity extends BaseActivity {
+public class HomeActivity extends BaseActivity implements View.OnClickListener {
+
+    @BindView(R.id.tv_cal)
+    TextView tvCal;
+    @BindView(R.id.iv_food)
+    ImageView ivFood;
+    @BindView(R.id.iv_water)
+    ImageView ivWater;
+    @BindView(R.id.iv_bmi)
+    ImageView ivBmi;
+    @BindView(R.id.iv_about)
+    ImageView ivAbout;
+    @BindView(R.id.iv_profile)
+    ImageView ivProfile;
+    @BindView(R.id.iv_logout)
+    ImageView ivLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +37,14 @@ public class HomeActivity extends BaseActivity {
         setContentView(R.layout.activity_home);
         // set default value
         DefaultValue.waterAlarm(this);
+        tvCal.setText(SharedPrefManager.getString(Constants.CALORIES) + " cal");
+        ivFood.setOnClickListener(this);
+        ivWater.setOnClickListener(this);
+        ivBmi.setOnClickListener(this);
+        ivAbout.setOnClickListener(this);
+        ivProfile.setOnClickListener(this);
+        ivProfile.setOnClickListener(this);
+
     }
 
     @Override
@@ -26,5 +55,13 @@ public class HomeActivity extends BaseActivity {
     @Override
     public void endLoading() {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.iv_food:
+
+        }
     }
 }
